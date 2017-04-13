c669c11b080ad159f678aaa8e41ce973618b7e43
sklalaludek <pattiegolis@gmail.com>
2017-04-13 18:30:32 +0200

    add simple slider

--- 5_SimpleSlider/js/app.js
+++ 5_SimpleSlider/js/app.js
@@ -8,19 +8,19 @@ $(document).ready(function() {
         imgW = allLi.eq(0).width(),
         index = 0;
 
-        slides.css('width', imgW*6);
+        slides.css('width', imgW*6 + 40);
 
         next.on('click', function(){
             index++;
-            if(index > allLi.length) {
-                index = 1;
+            if(index >= allLi.length) {
+                index = 0;
             }
             slides.animate({left: '-=' + imgW}, 200);
         });
         prev.on('click', function(){
             index--;
-            if(index < allLi.length - 1) {
-                index = 1;
+            if(index < 0) {
+                index = allLi.length - 1;
             }
             slides.animate({left: '+=' + imgW}, 200);
         });
